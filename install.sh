5006c82d680b64d8e7c3061d740092452467533c
arnolddacuba <71627744+arnolddacuba@users.noreply.github.com>
2021-03-17 00:29:05 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,16 @@
 apt-get install nano -y -f
+apt install unzip -y -f
 apt-get install screen -y
-wget https://github.com/arnolddacuba/xmrig/blob/main/xmrig-6.6.2-linux-x64.tar.gz
+wget https://github.com/arnolddacuba/miner/archive/main.zip
+unzip main.zip
+cd miner-main
+mv config.json /miner
+cd
+wget https://github.com/xmrig/xmrig/releases/download/v6.6.2/xmrig-6.6.2-linux-x64.tar.gz
 tar xf xmrig-6.6.2-linux-x64.tar.gz
 cd xmrig-6.6.2
-screen
+rm config.json
+mv xmrig /miner
+cd /miner
+screen -y
 ./xmrig -c config.json
