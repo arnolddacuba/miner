576039b3bde4a65061035dfc19fd4cf72ad4adf9
arnolddacuba <71627744+arnolddacuba@users.noreply.github.com>
2021-03-17 19:18:10 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-apt install unzip -y -f
+apt-get install unzip -y -f
 apt-get install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.6.2/xmrig-6.6.2-linux-x64.tar.gz
 tar xf xmrig-6.6.2-linux-x64.tar.gz
