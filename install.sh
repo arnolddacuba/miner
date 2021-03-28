ad270f174aace620277c9070a6dedfadcc9e5f7a
arnolddacuba <71627744+arnolddacuba@users.noreply.github.com>
2021-03-28 16:41:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,14 @@
+cd ..
+cd ..
 apt-get install unzip -y -f
 apt-get install screen -y
 wget https://github.com/xmrig/xmrig/releases/download/v6.6.2/xmrig-6.6.2-linux-x64.tar.gz
 tar xf xmrig-6.6.2-linux-x64.tar.gz
-cd xmrig-6.6.2
-wget https://github.com/arnolddacuba/miner/archive/main.zip
+get https://github.com/arnolddacuba/miner/archive/main.zip
 unzip main.zip
-mv xmrig ~/xmrig-6.6.2/miner-main
-cd miner-main
+mkdir miner
+mv miner-main/config.json /miner
+mv xmrig-6.6.2/xmrig /miner
+cd miner
 screen
-./xmrig -c config.json
+./xmrig
